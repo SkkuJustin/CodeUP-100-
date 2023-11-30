20c0f634777fee6d7535d5d5c1d8cbc5a823ae64
Woojoong Jeon <wos8000@gmail.com>
2023-11-30 16:30:27 +0900

    structure

---
+++ #65 Basic_structure(1).py
@@ -0,0 +1,14 @@
+# structure는 if / while / for 문을 이용하는 것임
+
+# 정수 3개 입력받아 짝수만 출력하기
+
+a,b,c = map(int,input().split())
+
+if a%2 == 0 :
+    print(a)
+    
+if b%2 == 0 :
+    print(b)
+    
+if c%2 == 0 :
+    print(c)
