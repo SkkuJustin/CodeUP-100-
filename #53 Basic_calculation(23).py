5e025596b7f3b0a2ead264ed537af2395338d93c
Woojoong Jeon <wos8000@gmail.com>
2023-11-30 15:20:54 +0900

    calculation

---
+++ #53 Basic_calculation(23).py
@@ -0,0 +1,10 @@
+# 참/거짓 바꾸기
+
+n = int(input())
+n = bool(n)
+print(not n)
+
+# not: 반대를 출력
+# and: 둘 다 True 일 경우 T / 하나라도 F 일 경우 F
+# or : 하나라도 True 일 경우 T / 둘 다 F라면 F
+# T and T // T or T // T or F == T
